70ec490d8923a86beb8b92f6c2516fa48f26c494
Saymon21 <158528583+Saymuu@users.noreply.github.com>
2024-10-21 16:29:55 -0300

    Add files via upload

---
+++ Questao2_POO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Montador montador = new Montador();
+
+        montador.MontarVentilador();
+
+        Console.WriteLine("Pilha original:");
+        montador.ExibirPilha();
+
+        Console.WriteLine("\nSubstituindo a peça 'Hélice' por 'Hélice Nova'...");
+        montador.SubstituirPeca("Hélice Quebrada", "Hélice Nova");
+
+        Console.WriteLine("\nPilha após a substituição:");
+        montador.ExibirPilha();
+    }
+}
